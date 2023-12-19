cb33ea229f6c25db2aae18b1c6ea3c80f2fa9d15
Mike Ramos <michaelangeloram8@gmail.com>
2023-12-18 21:32:06 -0600

    test commit from windows

--- models/product.js
+++ models/product.js
@@ -47,6 +47,7 @@ class Product {
 
   // new comment here
   // another comment here bro!
+  // but yet here's another test commit!
 
   static findByPk(prodId) {
     // first to get access to database
