feb945aed7bd223fbd720a05e03cc1fea53a6b6d
Mike Ramos <michaelangeloram8@gmail.com>
2023-12-18 21:17:29 -0600

    test commit

--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
-const mongodb = require('mongodb');
-const getDb = require('../utils/database').getDb; // Need the getDb method to connect to database
+const mongodb = require("mongodb");
+const getDb = require("../utils/database").getDb; // Need the getDb method to connect to database
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -14,14 +14,14 @@ class Product {
     // to connect to MongoDB and get the 'client' object
     const db = getDb();
 
-
     // to save the product in the MongoDB database using the 'client' object
-    return db.collection('products')
+    return db
+      .collection("products")
       .insertOne(this)
-      .then(result => {
+      .then((result) => {
         console.log(result);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
@@ -33,18 +33,19 @@ class Product {
 
     // To query the database and fetch all products data
     return db
-      .collection('products')
+      .collection("products")
       .find()
       .toArray()
-      .then(products => {
+      .then((products) => {
         console.log(products);
         return products;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
-  };
+  }
 
+  // new comment here
 
   static findByPk(prodId) {
     // first to get access to database
@@ -52,18 +53,18 @@ class Product {
 
     // To query the database and fetch 1 product w/ 'product id'
     return db
-      .collection('products')
+      .collection("products")
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
-      .then(product => {
-        console.log(product)
+      .then((product) => {
+        console.log(product);
         return product;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
-  };
-};
+  }
+}
 
 // To export our Product model
 module.exports = Product;
