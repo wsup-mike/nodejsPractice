50f72a93cc32cf90fef44cf4319d14fe0b7f2f3c
Mike Ramos <michaelangeloram8@gmail.com>
2024-01-11 12:24:32 -0600

    New commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/404");
 // const mongoConnect = require("./utils/database").mongoconnect;
@@ -20,6 +21,13 @@ const { error } = require("console");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "superman",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6598cd9676cd0a620f50db18")
