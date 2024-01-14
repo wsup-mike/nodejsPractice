dca4a58c1f5487e9bad8013fd35b06c1c41759a1
Mike Ramos <michaelangeloram8@gmail.com>
2024-01-14 16:17:44 -0600

    das kommitz

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLoginPage = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").trim().split("=")[1];
@@ -20,14 +21,31 @@ exports.getSignupPage = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("6598cd9676cd0a620f50db18") // Copied from former middleware in app.js
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
-      });
+      if (!user) {
+        // means login failed
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password) // boolean
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
     })
     .catch((err) => {
       console.log(err);
@@ -45,16 +63,21 @@ exports.postSignup = (req, res, next) => {
         // then no need to create new user
         return res.redirect("/signup");
       }
-      // otherwise to create a new user since no existing one found
-      const user = new User({
-        email: email,
-        password: password,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect("login");
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          // to hash our password for security in the database
+          // otherwise to create a new user since no existing one found
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
     })
     .catch((err) => console.log(err));
 };
